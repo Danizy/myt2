c10065453baf619c1605b2ceda94e2d170dbb3f2
Daniel Nizynski <danielnizynski97@gmail.com>
2023-04-02 17:42:48 +0200

    remoed usuned code

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -55,18 +55,14 @@ async function createWindow() {
     },
   })
 
-  if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
+  if (process.env.VITE_DEV_SERVER_URL) {
+    // electron-vite-vue#298
     win.loadURL(url)
     // Open devTool if the app is not packaged
     win.webContents.openDevTools()
   } else {
     win.loadFile(indexHtml)
   }
-
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
 
   // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
@@ -118,4 +114,3 @@ ipcMain.handle('open-win', (_, arg) => {
     childWindow.loadFile(indexHtml, { hash: arg })
   }
 })
-
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { useState } from 'react'
 import Update from '@/components/update'
 import './App.scss'
 
-console.log(
-  '[App.tsx]',
-  `Hello world from Electron ${process.versions.electron}!`
-)
-
 function App() {
   const [count, setCount] = useState(0)
   return (
